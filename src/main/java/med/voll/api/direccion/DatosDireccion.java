6a9d5dc89996c720d14537af8dc7422f50571e4b
Boris <borisnandez93@gmail.com>
2024-12-02 16:23:55 -0600

    validaciones de datos en dto

--- src/main/java/med/voll/api/direccion/DatosDireccion.java
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -1,10 +1,18 @@
 package med.voll.api.direccion;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record DatosDireccion(
+        @NotBlank
         String calle,
+        @NotNull
         Integer numero,
+        @NotBlank
         String complemento,
+        @NotBlank
         String distrito,
+        @NotBlank
         String ciudad
 ) {
 }
