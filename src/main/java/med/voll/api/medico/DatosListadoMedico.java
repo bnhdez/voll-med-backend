c1a2c84866b03ed18aaaa12436f0c1939772d0e9
Boris <borisnandez93@gmail.com>
2024-12-02 18:26:37 -0600

    actualizar datos

--- src/main/java/med/voll/api/medico/DatosListadoMedico.java
+++ src/main/java/med/voll/api/medico/DatosListadoMedico.java
@@ -1,12 +1,13 @@
 package med.voll.api.medico;
 
 public record DatosListadoMedico(
+        Long id,
         String nombre,
         String Especialidad,
         String documento,
         String email
 ) {
     public DatosListadoMedico(Medico medico){
-        this(medico.getNombre(), medico.getEspecialidad().toString(), medico.getDocumento(), medico.getEmail());
+        this(medico.getId(), medico.getNombre(), medico.getEspecialidad().toString(), medico.getDocumento(), medico.getEmail());
     }
 }
