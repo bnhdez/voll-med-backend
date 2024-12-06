a8c888fe49f1e18ee11f735d3e4a4a14d22d78c2
Boris <borisnandez93@gmail.com>
2024-12-05 21:04:38 -0600

    consulta, migration, controler, entidad, repo

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -3,7 +3,26 @@ package med.voll.api.domain.medico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
+
+    @Query(value = """
+        select * from medicos m
+        where
+        m.activo = 1
+        and
+        m.especialidad = :especialidad
+        and m.id not in (
+            select c.medico_id from consultas c
+            where
+            c.fecha = :fecha
+        )
+        order by rand()
+        limit 1
+        """, nativeQuery = true)
+    Medico elegirMedicoAleatorioDisponibleEnLaFecha(Especialidad especialidad, LocalDateTime fecha);
 }
